2ed022a0d60c0096e3b3be26a0729fda6cb1fdf8
rrogg <rene.rogge@mailbox.org>
2024-06-12 16:26:39 +0200

    Disable Welcome Screen and Clean Scratch Buffer
    The welcome screen is not needed after some time of using Emacs.
    Empty the *scratch* buffer to make it easy to save the content in this
    buffer to a file without needing to delete the default message in there.

--- early-init.el
+++ early-init.el
@@ -1,6 +1,9 @@
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
+(setq inhibit-startup-screen t
+      initial-scratch-message nil)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
