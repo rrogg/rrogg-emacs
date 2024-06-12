11ef2077014cfc869356a21bc6260fd3e11a0955
rrogg <rene.rogge@mailbox.org>
2024-06-12 23:56:00 +0200

    Wrap Long Lines and End Sentences with a Space
    Improve readability of text by wrapping lines at length of 70.
    characters. Don't wrap lines in programming modes.
    Sentences end with a single space.

--- init.el
+++ init.el
@@ -9,3 +9,16 @@
   (setq tab-first-completion 'word-or-paren-or-punct)
   (setq-default tab-width 4
                 indent-tabs-mode nil))
+
+(use-package text-mode
+  :ensure nil
+  :mode "\\`\\(README\\|CHANGELOG\\|COPYING\\|LICENSE\\)\\'"
+  :hook
+  (text-mode . turn-on-auto-fill)
+  :config
+  (setq-default fill-column 70 )
+  (setq sentence-end-double-space nil)
+  (setq sentence-end-without-period nil)
+  (setq colon-double-space nil)
+  (setq use-hard-newlines nil)
+  (setq adaptive-fill-mode t))
