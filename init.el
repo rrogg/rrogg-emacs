c4bd7a3e945d1300e43f87b093a45a7d27eb987c
rrogg <rene.rogge@mailbox.org>
2024-06-13 01:30:27 +0200

    Setup magit and implement commit guide
    Defaults for magit don't need to be touched for now.
    Implements a guide on how to write good commit messages.

--- init.el
+++ init.el
@@ -22,3 +22,22 @@
   (setq colon-double-space nil)
   (setq use-hard-newlines nil)
   (setq adaptive-fill-mode t))
+
+(use-package magit
+  :ensure t
+  :bind ("C-c g" . magit-status)
+  :init
+  (setq magit-define-global-key-bindings nil)
+  :config
+  (setq git-commit-summary-max-length 50)
+  (setq git-commit-style-convention-checks '(non-empty-second-line))
+  (setq git-commit-fill-column 70)
+  (defun rrogg-git-commit-setup()
+    (insert "#1. Capitalize the subject line
+#2. Do not end the subject line with a period
+#3. Use the present tense imperative mood in the subject line and body
+#4. Use the body to explain what and why vs. how
+#-----------------------------------------------
+#If applied, this commit will …
+"))
+  (add-hook 'git-commit-setup-hook #'rrogg-git-commit-setup))
