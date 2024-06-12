e5f4c3560eaf6e683c7603008ae74ac12f6ce966
rrogg <rene.rogge@mailbox.org>
2024-06-12 23:37:53 +0200

    Set TAB to Indent then Complete
    Never insert an actual tab, only spaces.
    The =TAB= key will try to indent the text first and then complete at point.

--- init.el
+++ init.el
@@ -1,2 +1,11 @@
 (setq make-backup-files nil)
 (setq create-lockfiles nil)
+
+(use-package emacs
+  :ensure nil
+  :demand t
+  :config
+  (setq tab-always-indent 'complete)
+  (setq tab-first-completion 'word-or-paren-or-punct)
+  (setq-default tab-width 4
+                indent-tabs-mode nil))
