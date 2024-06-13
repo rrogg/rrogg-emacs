d1a375083d4a82125792da58ff0e64cf984732a1
rrogg <rene.rogge@mailbox.org>
2024-06-13 16:08:59 +0200

    Manage Packages with use-package.el
    Retrieving, initializing and configuring packages is done via the
    macro use-package.el. It provides an uniform and efficient way of
    dealing with packages.

--- init.el
+++ init.el
@@ -23,6 +23,18 @@
   (setq use-hard-newlines nil)
   (setq adaptive-fill-mode t))
 
+(setq package-archives
+      '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
+        ("gnu-elpa-devel" . "https://elpa.gnu.org/devel/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ("melpa" . "https://melpa.org/packages/")))
+
+;; Highest number gets priority (what is not mentioned has priority 0)
+(setq package-archive-priorities
+      '(("gnu-elpa" . 3)
+        ("melpa" . 2)
+        ("nongnu" . 1)))
+
 (use-package magit
   :ensure t
   :bind ("C-c g" . magit-status)
