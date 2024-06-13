90d894c4ae50e7ccc9c3507e496824310479c4e0
rrogg <rene.rogge@mailbox.org>
2024-06-13 14:53:55 +0200

    Display the current date and time on the mode line

--- init.el
+++ init.el
@@ -70,3 +70,22 @@
 (use-package delsel
   :ensure nil
   :hook (after-init . delete-selection-mode))
+
+(use-package time
+  :ensure nil
+  :hook (after-init . display-time-mode)
+  :config
+  (setq display-time-format " %a %e %b, %H:%M %Z ")
+  (setq display-time-interval 60)
+  (setq display-time-default-load-average nil)
+  (setq display-time-mail-directory nil)
+  (setq display-time-mail-function nil)
+  (setq display-time-use-mail-icon nil)
+  (setq display-time-mail-string nil)
+  (setq display-time-mail-face nil)
+  (setq display-time-string-forms
+        '((propertize
+           (format-time-string display-time-format now)
+           'face 'display-time-date-and-time
+           'help-echo (format-time-string "%a %b %e, %Y" now))
+          " ")))
